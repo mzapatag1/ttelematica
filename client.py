890cce510a7eed882144c7c5cb17ec80f7060cb7
Manuela Zapata <35736549+mzapatag1@users.noreply.github.com>
2021-03-15 17:38:42 -0500

    Update client.py

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
   
 port = 80 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', port))  
+s.connect(('localhost', port))  
   
 # receive data from the server  
 print (s.recv(1024) ) 
